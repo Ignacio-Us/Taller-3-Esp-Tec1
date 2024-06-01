2229da5dfe24513f0eefe56235a6146070e4631a
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-01 17:18:45 -0400

    Agregacion de nuevos assets
    Se agregaron nuevos assets correspondiente a la pagina Home que funcionara como presentacion y wiki del videojuego.

--- src/client/pages/HomePage.vue
+++ src/client/pages/HomePage.vue
@@ -1,10 +1,17 @@
 <template>
     <div>
         <Navbar />
+        <header class="p-5">
+            <h1 class="text-white text-center font-custom" >Nombre del juego</h1>
+        </header>
+
     </div>
 </template>
 
 <style scoped>
+.font-custom {
+    font-family: "Planes_ValMore", sans-serif;
+}
 </style>
 
 <script setup>
